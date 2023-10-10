c7951be71acb4aa2664c9484482263da4572a251
jarli <jarli@DENODEN>
2023-10-10 18:32:21 +0300

    Добавьте файлы проекта.

---
+++ DZ_25/DZ_25.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstring>
+#include <Windows.h>
+
+using namespace std;
+
+void changeSymbol(char* str)
+{
+    char* strPtr;
+    for (strPtr = str; *strPtr; strPtr++) {
+        if (*strPtr == '.') {
+            *strPtr = '!';
+        }
+    }
+}
+int NumSymb(char* str, char symb)
+{
+    char* strPtr;
+    int c = 0;
+    for (strPtr = str; *strPtr; strPtr++) {
+        if (*strPtr == symb)
+            c++;
+    }
+    return c;
+}
+
+int main()
+{
+    //1
+    char myStr[] = "Hello. world.";
+    cout << "My original string: " << myStr << endl;
+    changeSymbol(myStr);
+    cout << "My transformed string: " << myStr << endl;
+    
+    //2
+    char nStr[] = "Hello World!";
+    char mySymb;
+    cout << "Enter a symbol: ";
+    cin >> mySymb;
+    cout << "In the line: '" << nStr << "'";
+    cout << " number of symbols " << "'" << mySymb << "'" << " = " << NumSymb(nStr, mySymb) << endl;
+}
